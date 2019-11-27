bf3915ec9cd875f867d89e9018cacddb2bc3f1f5
Kytel Flux <aoritb@outlook.com>
2019-11-27 15:41:13 +0000

    Updates

---
+++ precache-manifest.ad62ac6b3dac5cebfc57734acb689a7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ef74cfd8541a9edf7ffb3a46291954",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "24754634a9c5a3a46290",
+    "url": "/my-portfolio/static/css/main.4d8ce256.chunk.css"
+  },
+  {
+    "revision": "b37a36187b1f0adac5e1",
+    "url": "/my-portfolio/static/js/2.ccb790eb.chunk.js"
+  },
+  {
+    "revision": "24754634a9c5a3a46290",
+    "url": "/my-portfolio/static/js/main.da3bf4dd.chunk.js"
+  },
+  {
+    "revision": "d85d27cac53a8e7a29fd",
+    "url": "/my-portfolio/static/js/runtime-main.9c4e7d6c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kytelflux/kytelflux.github.io/precache-manifest.cad90e591c97922f0f41c9140126a413.js"
+  "/my-portfolio/precache-manifest.ad62ac6b3dac5cebfc57734acb689a7d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kytelflux/kytelflux.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
